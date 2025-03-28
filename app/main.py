a1ff08245c09affb0ddc4428c1cd07bbf009c4bd
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-28 14:50:50 +0200

    Add employers and jobs endpoints.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from strawberry import Schema
 from strawberry.fastapi import GraphQLRouter
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from contextlib import asynccontextmanager
 
 from .gql.queries import Query
-from .db.database import prepare_database
+from .db.database import prepare_database, engine
+from .db.models import Employer as Employer_sql, Job as Job_sql
+from sqlalchemy.orm import Session
 
 
 schema = Schema(Query)
@@ -20,3 +22,22 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(graphql_app, prefix="/graphql")
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.get("/employers")
+def get_employers(session: Session = Depends(get_session)):
+    with Session(engine) as session:
+        employers = session.query(Employer_sql).all()
+    return employers
+
+
+@app.get("/jobs")
+def get_jobs(session: Session = Depends(get_session)):
+    with Session(engine) as session:
+        jobs = session.query(Job_sql).all()
+    return jobs
