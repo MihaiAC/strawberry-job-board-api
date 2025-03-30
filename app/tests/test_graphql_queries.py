4b14d23e175f71318c3e6042896d5d83bf3dac7a
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-30 23:31:23 +0300

    Add test for employer gql query.

--- app/tests/test_graphql_queries.py
+++ app/tests/test_graphql_queries.py
@@ -1,9 +1,9 @@
 import pytest
-from app.db.data import JOBS_DATA
+from app.db.data import JOBS_DATA, EMPLOYERS_DATA
 
 
 @pytest.mark.api
-@pytest.mark.integration
+@pytest.mark.query
 def test_get_all_jobs(test_client, graphql_endpoint):
     query = """
         query {
@@ -22,4 +22,26 @@ def test_get_all_jobs(test_client, graphql_endpoint):
     assert len(jobs) == len(JOBS_DATA)
     assert sorted([job["title"] for job in jobs]) == sorted(
         job["title"] for job in JOBS_DATA
+    )
+
+
+@pytest.mark.api
+@pytest.mark.query
+def test_get_all_employers(test_client, graphql_endpoint):
+    query = """
+    query {
+        employers {
+            name
+        }
+    }
+    """
+    response = test_client.post(graphql_endpoint, json={"query": query})
+    assert response is not None
+    assert response.status_code == 200
+
+    result = response.json()
+    employers = result["data"]["employers"]
+    assert len(employers) == len(EMPLOYERS_DATA)
+    assert sorted([employer["name"] for employer in employers]) == sorted(
+        employer["name"] for employer in EMPLOYERS_DATA
     )
