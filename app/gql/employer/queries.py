dd98ae856119143d87cf25dd3f9d9f874fd02e38
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-02 15:43:23 +0300

    Update employer queries and tests.

--- app/gql/employer/queries.py
+++ app/gql/employer/queries.py
@@ -2,30 +2,14 @@ import strawberry
 from strawberry.types import Info
 from typing import List, Optional
 from app.db.models import Employer as Employer_sql, Employer_gql
-from sqlalchemy.orm import joinedload
-from app.gql.utils import to_employer_gql
 
 
 @strawberry.type
 class EmployerQuery:
-    # Allow retrieval of an employer's jobs.
-    # Disallow retrieval of the employer's jobs' employer
-    # (not useful anyway, since a job can't have multiple employers).
     @strawberry.field
     def employers(self, info: Info) -> List[Employer_gql]:
-        db_session = info.context["db_session"]
-        employers = (
-            db_session.query(Employer_sql).options(joinedload(Employer_sql.jobs)).all()
-        )
-        return [to_employer_gql(employer, deep=True) for employer in employers]
+        return Employer_sql.fetch_and_transform_to_gql(info)
 
     @strawberry.field
     def employer(self, id: int, info: Info) -> Optional[Employer_gql]:
-        db_session = info.context["db_session"]
-        employer = (
-            db_session.query(Employer_sql)
-            .options(joinedload(Employer_sql.jobs))
-            .filter_by(id=id)
-            .first()
-        )
-        return to_employer_gql(employer, deep=True) if employer else None
+        return Employer_sql.fetch_and_transform_to_gql(info, id=id)
