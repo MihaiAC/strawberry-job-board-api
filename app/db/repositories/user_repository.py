0d5d84ed81bfd69919a94f00372ea2dccce1f274
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-03 19:58:56 +0300

    Add user repository as a layer between mutations and db operations.

---
+++ app/db/repositories/user_repository.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+from app.db.models import User as User_sql
+from typing import Optional
+
+
+class UserRepository:
+    @staticmethod
+    def get_user_by_email(db_session: Session, email: str) -> Optional[User_sql]:
+        return db_session.query(User_sql).filter_by(email=email).first()
