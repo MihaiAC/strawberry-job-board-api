ed18cc77239c14b0bc1ccf3e5edca2b19e4fdb31
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-07 23:59:46 +0300

    Add token expiration tests.

--- app/tests/test_user_mutations.py
+++ app/tests/test_user_mutations.py
@@ -1,9 +1,6 @@
 import pytest
 from app.db.data import USERS_DATA
 from .utils import post_graphql
-from freezegun import freeze_time
-from app.settings.config import JWT_EXPIRATION_TIME_MINUTES
-from datetime import datetime, timezone, timedelta
 from app.errors.error_messages import (
     USER_ALREADY_EXISTS,
     INSUFFICIENT_PRIVILEGES,
@@ -253,47 +250,3 @@ def test_add_new_admin_auth_as_admin(
     assert user["email"] == email
     assert user["role"] == role
     assert user["id"] == len(USERS_DATA) + 1
-
-
-@pytest.mark.api
-@pytest.mark.mutation
-@pytest.mark.auth
-def test_add_new_admin_auth_as_admin_token_expired(
-    test_client,
-    graphql_endpoint,
-    admin_header,
-    db_session,
-):
-    username = "New User"
-    email = "new_email@example.com"
-    password = "newpass123"
-    role = "admin"
-    query = f"""
-    mutation {{
-        addUser(
-            username: "{username}",
-            email: "{email}",
-            password: "{password}",
-            role: "{role}"
-        ) {{
-            id
-            username
-            email
-            role
-        }}
-    }}
-    """
-    expiration_time = datetime.now(timezone.utc) + timedelta(
-        minutes=JWT_EXPIRATION_TIME_MINUTES + 1
-    )
-    with freeze_time(expiration_time):
-        result = post_graphql(
-            test_client,
-            graphql_endpoint,
-            query,
-            headers=admin_header,
-        )
-
-    assert "errors" in result
-    assert result["errors"][0]["message"] == INSUFFICIENT_PRIVILEGES
-    assert_no_new_user_added(db_session)
