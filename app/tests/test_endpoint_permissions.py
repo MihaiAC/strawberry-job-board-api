12733e32083cd8d427e31ef636d01015da712e0f
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-07 19:31:02 +0300

    Refactor permission tests to correctly work with adding objects.

--- app/tests/test_endpoint_permissions.py
+++ app/tests/test_endpoint_permissions.py
@@ -1,12 +1,13 @@
 import pytest
 from .utils import BaseQueries, post_graphql
+from app.auth.roles import Role
 
 
 # TODO: Maybe should add token expiration tests here.
-@pytest.mark.auth
-@pytest.mark.parametrize(
-    "query, permissions",
-    [
+
+
+def generate_permission_tests():
+    cases = [
         (BaseQueries.APPLICATIONS, [False, True, True]),
         (BaseQueries.LOGIN, [True, False, False]),
         (BaseQueries.UPDATE_EMPLOYER, [False, False, True]),
@@ -19,18 +20,41 @@ from .utils import BaseQueries, post_graphql
         (BaseQueries.QUERY_JOB_BY_ID, [True, True, True]),
         (BaseQueries.QUERY_ALL_USERS, [False, True, True]),
         (BaseQueries.CREATE_APPLICATION, [False, True, False]),
-    ],
+        (BaseQueries.ADD_USER, [True, False, True]),
+    ]
+
+    for query, (unauth, user, admin) in cases:
+        yield (query, Role.UNAUTHENTICATED, unauth)
+        yield (query, Role.USER, user)
+        yield (query, Role.ADMIN, admin)
+
+
+@pytest.mark.auth
+@pytest.mark.parametrize(
+    "query, role, expected_permission",
+    generate_permission_tests(),
 )
 def test_permissions(
-    test_client, graphql_endpoint, user_header, admin_header, query, permissions
+    test_client,
+    graphql_endpoint,
+    user_header,
+    admin_header,
+    query,
+    role,
+    expected_permission,
 ):
-    # unauth_has_permission, user_has_permission, admin_has_permission
-    # = permissions
+    # Set headers.
+    if role == Role.UNAUTHENTICATED:
+        headers = ""
+    elif role == Role.USER:
+        headers = user_header
+    elif role == Role.ADMIN:
+        headers = admin_header
+    else:
+        pytest.fail(f"Unknown role {role}")
 
-    for idx, header in enumerate(["", user_header, admin_header]):
-        result = post_graphql(test_client, graphql_endpoint, query, headers=header)
-        has_permission = permissions[idx]
-        query_was_successful = "errors" not in result
-        assert (
-            has_permission == query_was_successful
-        ), f"{idx} {has_permission} {query_was_successful}"
+    result = post_graphql(test_client, graphql_endpoint, query, headers=headers)
+    query_successful = "errors" not in result
+    assert (
+        query_successful == expected_permission
+    ), f"{role} access for query {query} expected {expected_permission} but got {query_successful}"
