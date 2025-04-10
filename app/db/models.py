2bbbc8473c853b33ca922be40989123fed82cbac
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-10 16:23:10 +0300

    Apply different filters based on filter_by_attrs.

--- app/db/models.py
+++ app/db/models.py
@@ -8,7 +8,10 @@ from sqlalchemy.orm import (
     relationship,
     class_mapper,
     Session,
+    Query,
 )
+
+from sqlalchemy import and_, or_
 
 from sqlalchemy import String, ForeignKey, UniqueConstraint
 from typing import List, Dict, Self
@@ -30,26 +33,90 @@ class Base(DeclarativeBase):
             return []
 
     @classmethod
+    def apply_single_attr_multivalued_filter(
+        cls,
+        query: Query,
+        attr_name: str,
+        attr_values: list,
+    ) -> Query:
+        query = query.filter(getattr(cls, attr_name).in_(attr_values))
+        return query
+
+    @classmethod
+    def apply_multi_attr_multivalued_filter(
+        cls,
+        query: Query,
+        attr_names: tuple[str, ...],
+        attr_values: list[tuple],
+    ) -> Query:
+        conditions = []
+        for values in attr_values:
+            condition_parts = []
+            for attr, value in zip(attr_names, values):
+                condition_parts.append(getattr(cls, attr) == value)
+            conditions.append(and_(*condition_parts))
+        return query.filter(or_(*conditions))
+
+    @classmethod
+    def apply_multi_attr_singlevalued_filter(
+        cls,
+        query: Query,
+        filter_by_attrs: dict,
+    ) -> Query:
+        for attr_name, attr_value in filter_by_attrs.items():
+            query = query.filter(getattr(cls, attr_name) == attr_value)
+        return query
+
+    @classmethod
     def get_all(
         cls: Self,
         db_session: Session,
-        filter_by_attrs: Dict[str, any] = {},
-        **kwargs,
+        filter_by_attrs: dict = {},
     ) -> List[Self]:
         """
         cls: Current SQLAlchemy class.
         db_session: Current session object.
-        selected_fields: Argument with the same name from the strawberry.Info
-        object, cast as string; extracting FKs to join on from it - ideally should have a parser
-        do this, but it's out of the scope of this project.
-        gql: Whether to cast the returned SQL objects to their equivalent strawberry type
         filter_by_attrs: Attributes to filter on.
-        ignore_fields: Again a misnomer, FKs to not join on (has to do with auth).
+        Dictionary, which can be:
+        1. Empty (no filtering needed);
+        2. In the format {attr_name: attr_val (scalar)};
+        Will add a filter for each (attr_name, attr_val) pair.
+        3. In the format {attr_name: [attr_val] (list)};
+        Must have only one entry. Finds all objects which have an attr_name
+        equal to one of the values in the list.
+        4. In the format {(attr_name1, attr_name2, ...): [(val1_ii, val2_ii, ...)]}
+        Must have only one entry.
+        Will find all objects whose values of (attr_name1, attr_name2, ...) match
+        one of the tuples in the list.
         """
         query = db_session.query(cls)
 
-        for attr_name, attr_value in filter_by_attrs.items():
-            query = query.filter(getattr(cls, attr_name) == attr_value)
+        if len(filter_by_attrs) > 0:
+            first_attr_key = next(iter(filter_by_attrs))
+            if isinstance(filter_by_attrs[first_attr_key], list):
+                if len(filter_by_attrs) > 1:
+                    raise Exception("filter_by_attrs has incorrect format.")
+                if isinstance(first_attr_key, str):
+                    # Format 3.
+                    attr_name = first_attr_key
+                    attr_values = filter_by_attrs[attr_name]
+                    query = cls.apply_single_attr_multivalued_filter(
+                        query,
+                        attr_name,
+                        attr_values,
+                    )
+                else:
+                    # Format 4.
+                    attr_names = first_attr_key
+                    attr_values = filter_by_attrs[attr_name]
+                    query = cls.apply_multi_attr_multivalued_filter(
+                        query,
+                        attr_names,
+                        attr_values,
+                    )
+            else:
+                # Format 2.
+                query = cls.apply_multi_attr_singlevalued_filter(query, filter_by_attrs)
 
         result_objs = query.all()
         return result_objs
